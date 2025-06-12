9868599a9969623d525f76a77046a43839387b57
kavinda-100 <kavindarathnayake100@gmail.com>
2025-06-12 12:35:10 +0530

    feat: add organization table schema with relations to price plan

---
+++ backend/src/db/schema/orgainzation.ts
@@ -0,0 +1,39 @@
+import * as pg from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { timestamps } from "./utils";
+import { pricePlanTable } from "./pricePlan";
+
+// organization table
+export const organizationTable = pg.pgTable(
+  "organizations",
+  {
+    id: pg.uuid("id").primaryKey().defaultRandom(),
+    name: pg.varchar("name", { length: 256 }).notNull(),
+    email: pg.varchar("email", { length: 256 }).notNull().unique(),
+    contactInfo: pg.varchar("contact_info", { length: 512 }).notNull(),
+    logoUrl: pg.varchar("logo_url", { length: 512 }),
+    region: pg.varchar("region", { length: 128 }).notNull(),
+    planId: pg
+      .uuid("plan_id")
+      .references(() => pricePlanTable.id)
+      .notNull(),
+    ...timestamps,
+  },
+  (table) => [
+    pg.uniqueIndex("org_name_idx").on(table.name),
+    pg.uniqueIndex("org_id_idx").on(table.id),
+    pg.uniqueIndex("org_email_idx").on(table.email),
+  ]
+);
+
+// Define relations for the organization table with the price plan table (one-to-one)
+// one organization has one price plan
+export const organizationRelations = relations(
+  organizationTable,
+  ({ one }) => ({
+    pricePlan: one(pricePlanTable, {
+      fields: [organizationTable.planId],
+      references: [pricePlanTable.id],
+    }),
+  })
+);
