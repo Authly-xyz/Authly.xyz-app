9f4660f8e41bf3b872695ce340b5f3fa06ccbe6f
kavinda-100 <kavindarathnayake100@gmail.com>
2025-06-17 21:30:10 +0530

    feat: enhance RegisterPage with Google authentication button and update layout

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,7 +1,40 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { FcGoogle } from "react-icons/fc";
+
 export default function RegisterPage() {
   return (
-    <section>
-      <h1>Welcome to the register page!</h1>
+    <section className="container mx-auto max-w-xl">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center text-xl lg:text-2xl">
+            Welcome to the Authly!
+          </CardTitle>
+          <CardDescription>
+            Authly is an authentication provider for your React app. It provides
+            a simple and secure way to manage user authentication and
+            authorization.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col items-center gap-4">
+          <Button className="w-full" variant={"outline"}>
+            <FcGoogle className="mr-2 size-5" />
+            Continue with Google
+          </Button>
+        </CardContent>
+        <CardFooter>
+          <p className="text-foreground/80 text-[12px]">
+            Verified Authentication with OAuth.2
+          </p>
+        </CardFooter>
+      </Card>
     </section>
   );
 }
