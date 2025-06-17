35a7d1aa3f086799da32dc1f7d1bc8582cf4fd8a
kavinda-100 <kavindarathnayake100@gmail.com>
2025-06-17 20:49:24 +0530

    init schadcn ui components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function HomePage() {
@@ -5,6 +6,7 @@ export default function HomePage() {
     <main>
       <h1>Welcome to the Home Page</h1>
       <Link href="/about">Go to About Page</Link>
+      <Button>Click Me</Button>
     </main>
   );
 }
